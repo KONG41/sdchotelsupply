069371c8e425a87d6cc522ce1826ab776a9fbc74
KONG41 <sokkongchan@gmal.com>
2024-01-28 15:27:50 +0700

    menu close when press link to new menu

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -11,12 +11,13 @@ import {
   NavbarMenuToggle,
   NavbarMenu,
   NavbarMenuItem,
-  DropdownItem, DropdownTrigger, Dropdown, DropdownMenu
+  DropdownItem, DropdownTrigger, Dropdown, DropdownMenu,
+  Link
 } from "@nextui-org/react";
 import logo from "~/assets/logo_t.png";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import Link from "next/link";
+// import Link from "next/link";
 import { GoChevronDown } from "react-icons/go";
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
@@ -26,12 +27,12 @@ const Header = () => {
   console.log(menuData)
   return (
     // <div className="container max-w-[1268px] mx-auto">
-    <Navbar onMenuOpenChange={setIsMenuOpen} maxWidth='full' isBordered>
+    <Navbar onMenuOpenChange={setIsMenuOpen}  isMenuOpen={isMenuOpen} maxWidth='full' isBordered>
       <div className="container max-w-[1268px] mx-auto flex items-center">
           <NavbarContent>
             <NavbarMenuToggle
               aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-              className="sm:hidden"
+              className="sm:hidden h-[20px]"
             />
             <NavbarBrand>
               <Link href="/"><Image src={logo} alt="logo png" className="w-[150px]" /></Link>
@@ -77,6 +78,7 @@ const Header = () => {
                               pathname === subMenuItem.description ? "text-[#DB2230]" : "text-[black]"
                             }
                             href={item.description +"?cat="+subMenuItem.id}
+                          
                           >
                             {subMenuItem.name}
                           </Link>
@@ -96,8 +98,8 @@ const Header = () => {
                    className={` ${pathname === item.description ? "text-[#DB2230]" : "text-[black]"} p-0 bg-transparent data-[hover=true]:bg-transparent text-md uppercase rounded-none`}
                   >
                      <Link
-                      
                       href={`${item.description}`}
+                      className="text-black"
                     >
                       {item.name}
                     </Link>
@@ -108,6 +110,7 @@ const Header = () => {
             )}
             
           </NavbarContent>
+          
           <NavbarContent justify="end">
             <NavbarItem>
               <Button
@@ -120,8 +123,9 @@ const Header = () => {
               </Button>
             </NavbarItem>
           </NavbarContent>
+
           <NavbarMenu>
-            { menuData && menuData.map((item, index) => (
+            {/* { menuData && menuData.map((item, index) => (
               <NavbarMenuItem key={`${item}-${index}`}>
                 <Link
                   className={`w-full ${
@@ -132,7 +136,70 @@ const Header = () => {
                   {item.name}
                 </Link>
               </NavbarMenuItem>
-            ))}
+            ))} */}
+            { menuData && menuData.map((item, index) => 
+              item.status == "active" && (
+                item.subMenus.length > 0 ? 
+                <Dropdown key={`${item}-${index}`}>
+                  <NavbarItem>
+                    <DropdownTrigger>
+                      <Button
+                       disableRipple
+                       endContent={<GoChevronDown />}
+                      
+                       className={` ${pathname === item.description ? "text-[#DB2230]" : "text-[black]"} p-0 bg-transparent data-[hover=true]:bg-transparent text-md uppercase rounded-none focus-visible:outline-none justify-start`}
+                      >
+                        {item.name}
+                      </Button>
+                      
+                    </DropdownTrigger>
+                  </NavbarItem>
+                  <DropdownMenu
+                    aria-label="ACME features"
+                    className="min-w-fit rounded-none"
+                    itemClasses={{
+                      base: "gap-4",
+                    }}
+                  > 
+                    { item.subMenus.filter(subMenu =>  subMenu.status === 'active').map(subMenuItem => 
+                      <DropdownItem
+                        key={subMenuItem.name}
+                        
+                      >
+                          <Link
+                            className={
+                              pathname === subMenuItem.description ? "text-[#DB2230]" : "text-[black]"
+                            }
+                            href={item.description +"?cat="+subMenuItem.id}
+                          >
+                            {subMenuItem.name}
+                          </Link>
+                      </DropdownItem>
+                    )
+                    
+                    }
+                    
+                    
+                  </DropdownMenu>
+                </Dropdown> 
+                
+                :
+                <NavbarItem key={`${item}-${index}`}>
+                  <Button
+                   disableRipple
+                   className={` ${pathname === item.description ? "text-[#DB2230]" : "text-[black]"} p-0 bg-transparent data-[hover=true]:bg-transparent text-md uppercase rounded-none justify-start`}
+                  >
+                     <Link
+                      href={`${item.description}`}
+                      className="text-black"
+                    >
+                      {item.name}
+                    </Link>
+                  </Button>
+                 
+                </NavbarItem>
+              )
+            )}
           </NavbarMenu>
       </div>
     </Navbar>
