5f5bfdb7958b0acc5effefdb3ea65910ce39d63b
KONG41 <sokkongchan@gmal.com>
2024-01-28 14:46:53 +0700

    responvie cover text

--- src/app/_components/CoverPage.tsx
+++ src/app/_components/CoverPage.tsx
@@ -12,7 +12,7 @@ const CoverPage = ({src,title,navigation}:cover) => {
     <div className='w-full h-[365px] relative'>
         <img src={`${src??"/"}`} alt='about us cover image' className='w-full h-full object-cover'/>
         <div className='absolute top-0 w-full h-full flex flex-col justify-center items-center text-white'>
-            <h1 className='text-[65px] font-bold'>{title}</h1>
+            <h1 className='sm:text-[65px] text-[55px] font-bold text-center'>{title}</h1>
             {navigation&&
                 <div className='text-[16px]'><span>Menu</span><IoIosArrowForward className='inline-block mx-3'/><span className='font-bold'>Submenu</span></div>
             }
