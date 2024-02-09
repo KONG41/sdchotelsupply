bedb8adb0f07e7b137e3642103232d4473bb03b4
KONG41 <sokkongchan@gmal.com>
2024-02-09 15:47:34 +0700

    change to old code

--- server.ts
+++ server.ts
@@ -1,19 +1,21 @@
-const express = require('express');
-const next = require('next');
+import { createServer } from "http";
+import { parse } from "url";
+import next from "next";
 
-const dev = process.env.NODE_ENV !== 'production';
+const port = parseInt(process.env.PORT || "3000", 10);
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-    const server = express();
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url!, true);
+    handle(req, res, parsedUrl);
+  }).listen(port);
 
-    server.all('*', (req, res) => {
-        return handle(req, res);
-    });
-
-    server.listen(3000, (err) => {
-        if (err) throw err;
-        console.log('> Ready on http://localhost:3000');
-    });
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? "development" : process.env.NODE_ENV
+    }`,
+  );
 });
